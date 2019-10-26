4ba2c9a759f412c6e298d66e5c88c8c32989cbf9
Mario Riveros <marioriveros@gmail.com>
2019-10-25 22:59:35 -0300

    Create PetTypesController.cs

---
+++ MyVet.Web/Controllers/API/PetTypesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyVet.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetTypesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PetTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
